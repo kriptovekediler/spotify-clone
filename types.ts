b6201fbf9cb09416df8e3004ef26b73433966788
Muhammed Talha Korkmaz <kriptovekediler@gmail.com>
2023-11-21 13:40:47 +0300

    added stripe

--- types.ts
+++ types.ts
@@ -24,7 +24,7 @@ export interface Price {
   product_id: string;
   active?: boolean;
   description?: string;
-  unit_amount?: string;
+  unit_amount?: number;
   currency?: string;
   type?: Stripe.Price.Type;
   interval?: Stripe.Price.Recurring.Interval;
